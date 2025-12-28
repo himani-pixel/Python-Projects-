d8a69b8772db2cc45129f85d6eaf506dfbf25c03
Himani Malankar <himanimalankar@gmail.com>
2025-12-29 01:28:59 +0530

    Update PDF Merger.py

--- Project 2 - PDF Merger/PDF Merger.py
+++ Project 2 - PDF Merger/PDF Merger.py
@@ -1,1 +1,14 @@
+merger = PdfWriter()
 
+pdfs = []
+n = int(input("How many pdfs do you want to merge? \n"))
+
+for i  in range(0 , n):
+    name = input(f"Enter the name of pdf {i + 1}: ")
+    pdfs.append(name)
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("MergedPDF.pdf")
+merger.close()
