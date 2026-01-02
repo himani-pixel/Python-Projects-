5ef27abb134bd63224f9b0403aae21e867b7dbe7
Himani Malankar <himanimalankar@gmail.com>
2026-01-02 20:40:46 +0530

    Update code.py

--- Project 4- QR Code Genrator using Python/code.py
+++ Project 4- QR Code Genrator using Python/code.py
@@ -1,1 +1,8 @@
+import qrcode
+url = input("Enter your link: ")
+filename = input("Filename you want to save it as: ")
+if not (filename.endswith(".jpg")):
+    filename = filename + ".jpg"
 
+img = qrcode.make(url)
+img.save(filename)
